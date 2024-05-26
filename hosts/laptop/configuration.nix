c16e0181d3f7de7c4d402225cdc074ea791c2d90
kamo104 <kamo.gabo@gmail.com>
2024-05-26 14:40:52 +0200

    kamo-laptop gen:428

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -81,6 +81,7 @@
 
   networking.firewall.allowedTCPPorts = [ 7777 25565 64738 8181 ]; # terraria, minecraft, mumble-server, musicboty
   networking.firewall.allowedUDPPorts = [ 7777 64738 8181 ]; # terraria, 
+  networking.firewall.enale = false;
 
   system.stateVersion = "23.11";
 }

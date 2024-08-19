01b995321dfa6a897a97c366fa0a47d56900ae02
kamo104 <kamo.gabo@gmail.com>
2024-08-19 21:40:33 +0200

    kamo-laptop gen:550

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -143,7 +143,8 @@
   services.avahi = {
     enable = true;
     openFirewall = true;
-    nssmdns = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
     publish = {
       enable = true;
       addresses = true;

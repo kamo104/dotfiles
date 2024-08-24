891153645a7920076048aa83d7ccbf14f21c18e8
kamo104 <kamo.gabo@gmail.com>
2024-08-24 15:50:05 +0200

    kamo-laptop gen:554

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -62,19 +62,18 @@
     };
   };
 
-  environment.systemPackages = with pkgs; 
-  let 
-  customKodi = pkgs.kodi-wayland.withPackages (p: with p; [
-      joystick
-      # jellyfin
-      kodi-retroarch-advanced-launchers
-      kodiPackages.steam-launcher
-  ]);
-  in
-  [
-    customKodi
-    # kodi-retroarch-advanced-launchers
-  ];
+  # environment.systemPackages = with pkgs; 
+  # let 
+  # customKodi = pkgs.kodi-wayland.withPackages (p: with p; [
+  #     joystick
+  #     # jellyfin
+  #     # kodi-retroarch-advanced-launchers
+  #     # kodiPackages.steam-launcher
+  # ]);
+  # in
+  # [
+  #   customKodi
+  # ];
 
 
 

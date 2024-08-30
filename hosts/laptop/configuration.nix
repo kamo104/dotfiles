c026fe98257e4d5e5bb6705cfa8b74d3377b156c
kamo104 <kamo.gabo@gmail.com>
2024-08-30 21:51:38 +0200

    kamo-laptop gen:566

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -22,6 +22,28 @@
       "${args.modules}/sunshine.nix"
     ];
 
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    # other Nginx options
+    virtualHosts."test.internal" =  {
+      # enableACME = true;
+      # forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://192.168.1.98:8181";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+};
+
+
   bluetooth.enable = true;
   locale.enable = true;
   cfonts.enable = true;

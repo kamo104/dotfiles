7bce916393654c4085015070fc388a1acdd10e05
kamo104 <kamo.gabo@gmail.com>
2024-09-01 12:04:55 +0200

    kamo-laptop gen:608

--- homeManagerModules/hypr/hyprland.nix
+++ homeManagerModules/hypr/hyprland.nix
@@ -24,8 +24,8 @@
         general = {
             after_sleep_cmd = "hyprctl dispatch dpms on";
             before_sleep_cmd = "playerctl pause; loginctl lock-session";
-            lock_cmd = "${pkgs.mpv}/bin/mpv --fs --loop /home/kamo/Videos/out-run.mov --hwdec=auto --taskbar-progress=no 2>/dev/null &
-              hyprlock && loginctl unlock-session";
+            lock_cmd = ''${pkgs.mpv}/bin/mpv --fs --loop /home/kamo/Videos/out-run.mov --hwdec=auto --taskbar-progress=no 2>/dev/null &
+              hyprlock && loginctl unlock-session'';
             unlock_cmd = "pkill -f '${pkgs.mpv}/bin/mpv --fs --loop /home/kamo/Videos/out-run.mov --hwdec=auto --taskbar-progress=no 2>/dev/null'";
           };
         listener = [

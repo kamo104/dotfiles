6f9e792b6f63bc80345e2402b49387cbd762ac94
kamo104 <kamo.gabo@gmail.com>
2024-09-01 20:45:44 +0200

    kamo-server gen:111

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -77,7 +77,7 @@ in
       # /share/kamo 10.100.1.0/24${defOpts}
       # /share/kamo 10.100.0.2(rw,insecure,fsid=0)
     exports = ''
-      /share      10.100.0.0/23(r,nohide,insecure,no_subtree_check,fsid=0)
+      /share      10.100.0.0/16(rw,nohide,insecure,no_subtree_check,fsid=0)
       /share/all  10.100.0.0/23(rw,nohide,insecure,no_subtree_check)
       /share/kamo 10.100.1.0/24(rw,nohide,insecure,no_subtree_check)
     '';

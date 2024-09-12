22fc22f1e05f8dfe0f19af69d8e1090cdaaae481
kamo104 <kamo.gabo@gmail.com>
2024-09-12 21:18:11 +0200

    kamo-server gen:146

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -104,7 +104,8 @@ in
       ];
     };
   };
-  security.pki.certificateFiles = [ (builtins.toPath "${builtins.toPath args.secrets}/pki/ca.crt") ];
+  security.pki.certificateFiles = [ "/etc/secrets/pki/ca.crt" ];
+  # security.pki.certificateFiles = [ "${args.secrets}/pki/ca.crt" ];
   services.nginx = {
     enable = true;
     user = "nginx";

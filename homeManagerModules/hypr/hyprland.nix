b8dabb0e97183fa960587626c772f5d379d46399
kamo104 <kamo.gabo@gmail.com>
2024-09-19 01:29:36 +0200

    kamo-laptop gen:761

--- homeManagerModules/hypr/hyprland.nix
+++ homeManagerModules/hypr/hyprland.nix
@@ -12,6 +12,7 @@
     wayland.windowManager.hyprland = {
       enable = true;
       xwayland.enable = true;
+      systemd.enable = true;
       extraConfig = ''${builtins.readFile ./hyprland.conf}'';
     };
     programs.hyprlock = {

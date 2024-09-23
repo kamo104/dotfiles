d8a141b32d2931729afd01d1910736f7f73f229a
Kamil Grzymkowski <kamil.grzymkowski@itti.com.pl>
2024-09-23 12:19:57 +0200

    work-laptop gen:1

--- gc.sh
+++ gc.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-nix-env --delete-generations +1 --profile $HOME/.local/state/nix/profiles/home-manager
-sudo nix-env --delete-generations +1 --profile /nix/var/nix/profiles/system
-# nix-store --gc
-# nix-store --optimise
+if [ "$NIX_INSTALL_TYPE" = "OS" ]; then
+  nix-env --delete-generations +1 --profile $HOME/.local/state/nix/profiles/home-manager
+  sudo nix-env --delete-generations +1 --profile /nix/var/nix/profiles/system
+elif [ "$NIX_INSTALL_TYPE" = "PM" ]; then
+  sudo nix profile wipe-history
+  nix profile wipe-history
+fi
+
+# run actual gc
 nix store gc
 # nix store optimise

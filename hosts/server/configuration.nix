6bc880d047f596966eb698e0429569b1ef6914b5
kamo104 <kamo.gabo@gmail.com>
2024-10-07 01:57:37 +0200

    kamo-server gen:224

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -91,6 +91,7 @@ in
   services.dnsmasq = {
     enable = true;
     settings = {
+      # TODO: do we really need to use 10.64.0.1 always?
       server = [ "10.64.0.1" "/duckdns.org/8.8.8.8" ];
       # TODO: check openvpn interoparability
       address = [
@@ -252,7 +253,7 @@ in
       address = [ "10.67.130.19/32" ];
       listenPort = 42070;
       privateKeyFile = "${args.secrets}/wg-keys/mullvad/private";
-      dns = [ "10.64.0.1" ];
+      # dns = [ "10.64.0.1" ];
       table = "off";
       postUp = ''
         ip route add default dev wg1 table wg1_table

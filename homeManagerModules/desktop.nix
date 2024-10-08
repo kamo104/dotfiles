0687dcf2f3b6bb418b96198f9ad09e43a931b4aa
kamo104 <kamo.gabo@gmail.com>
2024-10-08 21:24:12 +0200

    kamo-laptop gen:790

--- homeManagerModules/desktop.nix
+++ homeManagerModules/desktop.nix
@@ -185,9 +185,6 @@
     programs.fish = {
       enable = true;
       interactiveShellInit = ''
-        set fish_greeting
-        alias wakedesktop="wakeonlan 58:11:22:bc:ec:50"
-        alias kssh="kitten ssh"
         alias clock="bash -c 'while true; do tput clear; date +"%H : %M : %S" | ${pkgs.figlet}/bin/figlet ; sleep 1; done'"
       '';
     };

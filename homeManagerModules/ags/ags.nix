957bbd54189a25571670db936b52d85eb26f7a54
kamo104 <kamo.gabo@gmail.com>
2024-10-13 19:48:18 +0200

    kamo-laptop gen:825

--- homeManagerModules/ags/ags.nix
+++ homeManagerModules/ags/ags.nix
@@ -8,19 +8,17 @@
   config = lib.mkIf config.ags.enable {
     nixpkgs.overlays = [
       (final: prev: {
-        ags = inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.default;
+        # ags = inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.default;
         # ags = config.programs.ags.package;
-        # ags = config.programs.ags.finalPackage;
+        ags = config.programs.ags.finalPackage;
       })
     ];
     programs.ags = {
       enable = true;
       systemd.enable = true;
+      package = config.programs.ags.finalPackage;
 
-      # null or path, leave as null if you don't want hm to manage the config
       configDir = ./.;
-
-      # additional packages to add to gjs's runtime
       extraPackages = with pkgs; [
         gtksourceview
         webkitgtk

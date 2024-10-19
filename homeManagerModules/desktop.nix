fd199c03d69b9dc94424ead6a9b790b688b68e57
kamo104 <kamo.gabo@gmail.com>
2024-10-19 02:43:36 +0200

    kamo-laptop gen:872

--- homeManagerModules/desktop.nix
+++ homeManagerModules/desktop.nix
@@ -101,7 +101,7 @@
     programs.firefox = {
       enable = true;
       # package = pkgs.firefox-beta;
-      nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
+      nativeMessagingHosts = [ pkgs.firefoxpwa ];
       profiles = {
         kamo = {
           id = 0;

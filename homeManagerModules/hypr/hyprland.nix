dc283e91266073cc2a902d4c769d998433a3f415
kamo104 <kamo.gabo@gmail.com>
2024-10-19 03:22:00 +0200

    kamo-laptop gen:874

--- homeManagerModules/hypr/hyprland.nix
+++ homeManagerModules/hypr/hyprland.nix
@@ -31,7 +31,7 @@
         browser = "${pkgs.firefox}/bin/firefox";
         homeAssistant = ''${specialApp} "Home Assistant" "${browser} --new-window home-assistant.kkf.internal"'';
         immich = ''${specialApp} "Immich" "${browser} --new-window immich.kkf.internal"'';
-        jellyfin = ''${specialApp} "Jellyfin" "${browser} --new-window jellyfin.kkf.internal"'';
+        jellyfin = ''${specialApp} "Jellyfin" "row=$(${pkgs.firefoxpwa}/bin/firefoxpwa profile list | grep Jellyfin); row=''${row#* (}; row=''${row%*)}; ${pkgs.firefoxpwa}/bin/firefoxpwa site launch $row"'';
         messenger = "${browser} --new-window messenger.com";
         lock = "loginctl lock-session";
         ags_windows = [ "overview" "indicator0" "indicator1" "sideright" "osk" "session" "bar0" "bar1" ];

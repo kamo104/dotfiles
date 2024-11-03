cddeffefab8514935b6e93f3d71c9bdad0d0a531
kamo104 <kamo.gabo@gmail.com>
2024-11-03 11:39:41 +0100

    kamo-laptop gen:889

--- hosts/kamo-laptop/configuration.nix
+++ hosts/kamo-laptop/configuration.nix
@@ -137,7 +137,10 @@
   networking.firewall.allowedTCPPorts = [ 6881 ]; # deluge
   networking.firewall.allowedUDPPorts = [ 1900 6881 42069 ]; # upnp, deluge, wireguard 
 
-  services.zerotierone.enable = true;
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = ["1c33c1ced078606c"];
+  };
 
   networking.wg-quick.interfaces = {
     wg0 = {

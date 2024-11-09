1d8987163a528389744a299ed9ba1805808d6779
kamo104 <kamo.gabo@gmail.com>
2024-11-09 13:00:56 +0100

    kamo-laptop gen:896

--- nixosModules/sunshine.nix
+++ nixosModules/sunshine.nix
@@ -18,12 +18,12 @@
             auto-detach = "true";
             prep-cmd = [
               {
-                do = "${pkgs.hyprctl}/bin/hyprctl keyword monitor headless,1920x1080@60.00Hz,-1920x0,1";
+                do = "${pkgs.hyprland}/bin/hyprctl keyword monitor headless,1920x1080@60.00Hz,-1920x0,1";
                 # undo = "";
               }
               {
-                do = "${pkgs.hyprctl}/bin/hyprctl output create headless headless";
-                undo = "${pkgs.hyprctl}/bin/hyprctl output destroy headless";
+                do = "${pkgs.hyprland}/bin/hyprctl output create headless headless";
+                undo = "${pkgs.hyprland}/bin/hyprctl output destroy headless";
               }
             ];
           }

5689b9ee1c8f4b1f7efa2c2f173fafa50dbc392a
kamo104 <kamo.gabo@gmail.com>
2024-11-28 04:30:49 +0100

    kamo-laptop gen:903

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # hyprland = {
     #   url = "github:hyprwm/Hyprland";
     #   inputs.nixpkgs.follows = "nixpkgs";

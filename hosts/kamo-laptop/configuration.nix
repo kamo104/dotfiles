be459f02700b72b2e83ea862cd1f234475fc87e4
kamo104 <kamo.gabo@gmail.com>
2025-01-20 00:10:23 +0100

    kamo-laptop gen:916

--- hosts/kamo-laptop/configuration.nix
+++ hosts/kamo-laptop/configuration.nix
@@ -171,4 +171,14 @@
   };
 
   system.stateVersion = "23.11";
+  environment.systemPackages =
+  let
+    workingSignalPkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/e24b4c09e963677b1beea49d411cd315a024ad3a.tar.gz";
+      sha256 = "sha256-u1gk5I1an975FOAMMdS6oBKnSIsZza5ZKhaeBZAskVo=";
+    }) {};
+  in
+  [
+    workingSignalPkgs.signal-desktop
+  ];
 }

ffc9781f8d473b9f03015a8ef4bd83c75dfe4f4a
kamo104 <kamo.gabo@gmail.com>
2025-01-19 23:58:20 +0100

    kamo-laptop gen:916

--- hosts/kamo-laptop/home.nix
+++ hosts/kamo-laptop/home.nix
@@ -34,6 +34,7 @@
   let
     workingSignalPkgs = import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/e24b4c09e963677b1beea49d411cd315a024ad3a.tar.gz";
+      sha256 = "sha256-u1gk5I1an975FOAMMdS6oBKnSIsZza5ZKhaeBZAskVo=";
     }) {};
   in[
     workingSignalPkgs.signal-desktop

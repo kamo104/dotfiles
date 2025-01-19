56ed23173ba75241077c19650967b3d2fd8a93a0
kamo104 <kamo.gabo@gmail.com>
2025-01-20 00:06:09 +0100

    kamo-laptop gen:916

--- hosts/kamo-laptop/home.nix
+++ hosts/kamo-laptop/home.nix
@@ -36,8 +36,9 @@
       url = "https://github.com/NixOS/nixpkgs/archive/e24b4c09e963677b1beea49d411cd315a024ad3a.tar.gz";
       sha256 = "sha256-u1gk5I1an975FOAMMdS6oBKnSIsZza5ZKhaeBZAskVo=";
     }) {};
-  in[
-    workingSignalPkgs.signal-desktop
+  in
+  [
+    workingSignalPkgs.legacyPackages.x86_64-linux.signal-desktop
     keepassxc
 
     # jetbrains.idea-community

0878e313db2047a0f76d9ed34794542248d858dc
kamo104 <kamo.gabo@gmail.com>
2025-01-20 03:05:00 +0100

    kamo-server gen:264

--- hosts/kamo-server/configuration.nix
+++ hosts/kamo-server/configuration.nix
@@ -383,14 +383,22 @@ in
       postUp = ''
         ip route add 10.100.0.0/20 dev wg0 table wg1_table
         ip route add 192.168.1.0/24 dev ens18 table wg1_table
+        # enable forwarding to the wg1 interface
         ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-        # ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -s 10.100.0.0/23 -d 10.100.1.101 -j DNAT --to-destination 192.168.1.94
-        # ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.1.94 -d 10.100.0.0/23 -j MASQUERADE
+
+        # enable routing from server,kamo 10.100.1.101 == 192.168.1.94
+        ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -s 10.100.0.0/23 -d 10.100.1.101 -j DNAT --to-destination 192.168.1.94
+        # mask as the server ip
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/23 -d 192.168.1.94 -j SNAT --to-source 192.168.1.82
       '';
       postDown = ''
         ip route del 10.100.0.0/20 dev wg0 table wg1_table
         ip route del 192.168.1.0/24 dev ens18 table wg1_table
         ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+
+        ${pkgs.iptables}/bin/iptables -t nat -D PREROUTING -s 10.100.0.0/23 -d 10.100.1.101 -j DNAT --to-destination 192.168.1.94
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/23 -d 192.168.1.94 -j SNAT --to-source 192.168.1.82
+
       '';
       peers = [
         { # laptop

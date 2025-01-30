cfd44d06cba70c38c68c944fe1b86ca7afca6d39
Kamil Grzymkowski <kamil.grzymkowski@itti.com.pl>
2025-01-30 14:22:56 +0100

    work-laptop gen:110

--- hosts/work-laptop/home.nix
+++ hosts/work-laptop/home.nix
@@ -54,15 +54,9 @@
       ${pkgs.procps}/bin/pkill -f "${ovpn}"
       ${pkgs.procps}/bin/pkill -f "${ftvpn}"
     '') + "/bin/off";
-
-    ban = (pkgs.writers.writeBashBin "vban" '' 
-      pw-cli load-module -m libpipewire-module-vban-send audio.format="S16LE" audio.rate=48000 sess.name="work-audio" destination.ip="10.100.1.2" sess.latency.msec=10 &
-      pw-cli load-module -m libpipewire-module-vban-recv stream.props={audio.rate=48000 audio.format=S16LE} sess.name="work-samson" source.ip="10.100.1.2" sess.latency.msec=30
-    '') + "/bin/vban";
   in ''
     alias vpnOn="${on}"
     alias vpnOff="${off}"
-    alias vbanOn="${ban}"
     '';
 
  

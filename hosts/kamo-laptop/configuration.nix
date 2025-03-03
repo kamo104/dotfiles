09edc642dde49b775c22b4272f6fb8b792fe9e93
kamo104 <kamo.gabo@gmail.com>
2025-03-03 05:26:12 +0100

    kamo-laptop gen:937

--- hosts/kamo-laptop/configuration.nix
+++ hosts/kamo-laptop/configuration.nix
@@ -150,6 +150,9 @@
       listenPort = 42069;
       privateKeyFile = "${args.secrets}/wg-keys/internal/private";
       dns = ["10.100.0.1" "~kkf.internal"];
+      postUp = ''
+        ${pkgs.resolvectl}/bin/resolvectl domain wg0 '~kkf.internal'
+      '';
       peers = [
         {
           publicKey = "oT6pJKSYRfosjzNQ9nUNQiDDyDzZylVCCJ8ePNXwX0Y=";

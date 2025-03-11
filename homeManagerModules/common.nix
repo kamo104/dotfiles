57c0af656979a63773ace9016876179c966a2ec2
Kamil Grzymkowski <kamil.grzymkowski@itti.com.pl>
2025-03-11 13:49:16 +0100

    work-laptop gen:116

--- homeManagerModules/common.nix
+++ homeManagerModules/common.nix
@@ -30,7 +30,7 @@
       clock24 = true;
       terminal = "xterm";
       escapeTime = 0;
-      newSession = true;
+      # newSession = true;
       prefix = "C-a";
       extraConfig = ''
         bind c new-window -c "#{pane_current_path}"

0183f565305a4323a838b5d48fce2af59d69f007
kamo104 <kamo.gabo@gmail.com>
2025-03-15 13:53:13 +0100

    kamo-mac gen:41

--- hosts/kamo-mac/configuration.nix
+++ hosts/kamo-mac/configuration.nix
@@ -40,11 +40,11 @@
 
   networking.wg-quick.interfaces = {
     wg0 = {
-      autostart = true;
+      # autostart = true;
       address = [ "10.100.1.7/32" ];
-      listenPort = 42069;
+      # listenPort = 42069;
       privateKeyFile = "${args.secrets}/wg-keys/internal/private";
-      dns = ["10.100.0.1"];
+      # dns = ["10.100.0.1"];
       # postUp = ''
       #   ${pkgs.systemd}/bin/resolvectl domain wg0 '~kkf.internal'
       # '';

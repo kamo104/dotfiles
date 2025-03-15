0fc0ee126729fb3fe7697c4072b70af9b5c0f6c4
Kamil <kamo@kamils-macbook-air-1.home>
2025-03-15 04:21:42 +0100

    kamo-mac gen:3

--- flake.nix
+++ flake.nix
@@ -96,6 +96,7 @@
             {
               # nixpkgs = nixpkgsConfig;
               services.nix-daemon.enable = true;
+              security.pam.services.sudo_local.enable = true;
               security.pam.services.sudo_local.touchIdAuth = true;
 
               programs.fish.enable = true;

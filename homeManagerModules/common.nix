67c5329fa5d38bd24ae25f51025d0db590253667
kamo104 <kamo.gabo@gmail.com>
2025-03-15 16:12:26 +0100

    kamo-mac gen:47

--- homeManagerModules/common.nix
+++ homeManagerModules/common.nix
@@ -36,7 +36,7 @@
         bind c new-window -c "#{pane_current_path}"
         bind % split-window -h -c "#{pane_current_path}"
         bind '"' split-window -v -c "#{pane_current_path}"
-        set-option -g default-shell $SHELL
+        set-option -g default-shell /run/current-system/sw/bin/fish
       '';
     };
     

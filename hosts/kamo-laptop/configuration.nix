e0c3e3eb965c1fe25d18d70e98a5d9dba7a29e7f
kamo104 <kamo.gabo@gmail.com>
2025-04-10 22:54:05 +0200

    kamo-laptop gen:946

--- hosts/kamo-laptop/configuration.nix
+++ hosts/kamo-laptop/configuration.nix
@@ -84,14 +84,15 @@
   };
 
   services.avahi = {
-    nssmdns = true;
     enable = true;
+    nssmdns = true;
     ipv4 = true;
     ipv6 = true;
     publish = {
       enable = true;
       addresses = true;
       workstation = true;
+      userServices = true;
     };
   };
 

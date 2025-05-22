6bc876eadc7b3a141b6f51c0e53e9cf4b3aaa0b4
kamo104 <kamo.gabo@gmail.com>
2025-05-22 17:20:36 +0200

    kamo-server gen:283

--- hosts/kamo-server/configuration.nix
+++ hosts/kamo-server/configuration.nix
@@ -489,10 +489,10 @@ in
         #   publicKey = "hta1KqItY82z99Uimq7Ej674sIMbJ2EwRQXMnC96Omg=";
         #   allowedIPs = [ "10.100.13.3/32" ];
         # }
-        { # artur international challenges
-          publicKey = "y+KNixZQheNgTBzm33aO1nPn7aJnbJvghbZEbBAa83w=";
-          allowedIPs = [ "10.100.15.1/32" ];
-        }
+        # { # artur international challenges
+        #   publicKey = "y+KNixZQheNgTBzm33aO1nPn7aJnbJvghbZEbBAa83w=";
+        #   allowedIPs = [ "10.100.15.1/32" ];
+        # }
       ];
     };
   };

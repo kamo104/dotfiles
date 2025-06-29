206feff14effc9f69d88ecf5da2765d2df232f83
kamo104 <kamo.gabo@gmail.com>
2025-06-29 04:12:11 +0200

    kamo-laptop gen:989

--- hosts/kamo-laptop/configuration.nix
+++ hosts/kamo-laptop/configuration.nix
@@ -312,7 +312,7 @@
       address = [ "10.100.1.2/32" ];
       listenPort = 42069;
       privateKeyFile = "${args.secrets}/wg-keys/internal/private";
-      # dns = ["10.100.0.1"];
+      dns = ["10.100.0.1" "~kkf.internal"];
       postUp = ''
         ${pkgs.systemd}/bin/resolvectl domain wg0 '~kkf.internal'
       '';

293afa9df857d0e0d27426d54e5310a3e3a668f7
kamo104 <kamo.gabo@gmail.com>
2025-06-29 02:15:16 +0200

    kamo-laptop gen:984

--- nixosModules/common.nix
+++ nixosModules/common.nix
@@ -60,13 +60,20 @@ in
     environment.systemPackages = import "${args.modules}/common-pkgs.nix" {inherit pkgs customPkgs;};
     programs.fish.enable = true;
 
-    users.users = lib.genAttrs (config.common.users (user: {
+    users.users = lib.genAttrs config.common.users (user: {
       shell = pkgs.fish;
       isNormalUser = true;
       description = "${user}";
       extraGroups = [ "networkmanager" "wheel" "input" "video" "dialout" ];
       # packages = with pkgs; [];
-    }));
+    });
+    # users.users = lib.genAttrs (config.common.users (user: {
+    #   shell = pkgs.fish;
+    #   isNormalUser = true;
+    #   description = "${user}";
+    #   extraGroups = [ "networkmanager" "wheel" "input" "video" "dialout" ];
+    #   # packages = with pkgs; [];
+    # }));
 
     programs.git.enable = true;
     
